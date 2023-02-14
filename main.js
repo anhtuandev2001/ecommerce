10d1b9d57d518a021bca86e028d4c2d26b081d7a
Vũ Anh Tuấn <jun@KenMac.local>
2023-02-14 08:36:10 +0700

    updata code

--- main.js
+++ main.js
@@ -26,20 +26,8 @@ tabs.forEach((tab, index) => {
   };
 });
 
-var slideIndex = 1;
-showDivs(slideIndex);
+const btnScroll = document.querySelector('.footer__info-scroll')
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
+btnScroll.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+})
