5e2e73b68d22d069d0d05e5edc2297b902b789ee
Vũ Anh Tuấn <jun@KenMac.local>
2023-02-14 09:44:01 +0700

    commitupdate code

--- main.js
+++ main.js
@@ -8,26 +8,37 @@ navElemnt.onclick = function(){
     navigationElement.classList.toggle("display--flex")
 }
 
-const tabs = $$(".footer__item-tittle");
-const panes = $$(".footer__item-list");
+const footerTittles = $$(".footer__item-tittle");
+const footerItems = $$(".footer__item-list");
 
+
+
+footerTittles.forEach((tab, index) => {
+  const footerItem = footerItems[index];
+
+  tab.onclick = function () {
+    $(".footer__item-list.footer__item-list--active").classList.remove("footer__item-list--active");
+    $(".footer__item-tittle.footer__item--active").classList.remove("footer__item--active");
+    tab.classList.add("footer__item--active");
+    footerItem.classList.add("footer__item-list--active");
+
+
+  };
+});
+
+const tabs = $$(".section__6-ingredient");
+const panes = $$(".section__6-list");
 
 
 tabs.forEach((tab, index) => {
   const pane = panes[index];
 
   tab.onclick = function () {
-    $(".footer__item-list.footer__item-list--active").classList.remove("footer__item-list--active");
-    $(".footer__item-tittle.footer__item--active").classList.remove("footer__item--active");
-    tab.classList.add("footer__item--active");
-    pane.classList.add("footer__item-list--active");
-
-
+    console.log(tab)
+    $(".section__6-ingredient.section__6-ingredient--active").classList.remove("section__6-ingredient--active");
+    $(".section__6-list.section__6-list--active").classList.remove("section__6-list--active");
+    
+    this.classList.add("section__6-ingredient--active");
+    pane.classList.add("section__6-list--active");
   };
 });
-
-const btnScroll = document.querySelector('.footer__info-scroll')
-
-btnScroll.addEventListener('click', () => {
-  window.scrollTo(0, 0);
-})
